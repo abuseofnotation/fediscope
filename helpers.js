1e710f9ab8a0d7b65d3dee5f7303ebf6a3fae86c
Boris Marinov <marinovboris@protonmail.com>
2023-10-10 11:27:13 +0300

    stuff

--- helpers.js
+++ helpers.js
@@ -1,67 +1,74 @@
-const development = window.location.hostname === 'localhost'
+const development = window.location.hostname === "localhost";
+export const createElement =
+  (type) =>
+  ({ className, text, onClick, ...props }, children) => {
+    const div = document.createElement(type);
+    if (className) {
+      div.classList.add(className);
+    }
+    if (text) {
+      let textNode = document.createTextNode(text);
+      div.appendChild(textNode);
+    }
+    if (children) {
+      div.replaceChildren(...children);
+    }
+    if (onClick) {
+      div.addEventListener("click", onClick);
+    }
+    Object.keys(props).forEach((propName) => {
+      div.setAttribute(propName, props[propName]);
+    });
+    return div;
+  };
 
-export const createElement = (type) => ({className, text, onClick, ...props}, children) => {
-  const div = document.createElement(type)
-  if (className) {
-    div.classList.add(className);
-  }
-  if (text) {
-    let textNode = document.createTextNode(text);
-    div.appendChild(textNode);
-  }
-  if (children) {
-    div.replaceChildren(...children)
-  }
-  if (onClick) {
-    div.addEventListener('click', onClick)
-  }
-  Object.keys(props).forEach((propName) => {
-    div.setAttribute(propName, props[propName])
-  })
-  return div
-}
+// Functions for creating html elements
+// (Right now they create DOM elements directly, but they
+// can be made to work with with some virtual DOM lib
 
-  // Functions for creating html elements
-  // (Right now they create DOM elements directly, but they 
-  // can be made to work with with some virtual DOM lib
+export const div = createElement("div");
+export const button = createElement("button");
+export const span = createElement("span");
+export const input = createElement("input");
+export const a = createElement("a");
 
-export const div = createElement('div')
-export const button = createElement('button')
-export const span = createElement('span')
-export const input = createElement('input')
-export const a = createElement('a')
+const appName = "fediScope";
 
-const appName = "fediScope"
-  
-export const renderComponent = (component, state = JSON.parse(localStorage.getItem(appName)) || {} , params) => {
-  console.log('Rendering app with state', state)
-  document.getElementById("app")
-    .replaceChildren(component({
+export const renderComponent = (
+  component,
+  state = JSON.parse(localStorage.getItem(appName)) || {},
+  params,
+) => {
+  console.log("Rendering app with state", state);
+  document.getElementById("app").replaceChildren(
+    component({
       state,
       setState: (stateUpdate) => {
-        const newState = {...state, ...stateUpdate }
-        localStorage.setItem(appName, JSON.stringify(newState))
-        renderComponent(component, newState, params)
+        const newState = { ...state, ...stateUpdate };
+        localStorage.setItem(appName, JSON.stringify(newState));
+        renderComponent(component, newState, params);
       },
-      ...params
-    }))
-}
-export const get = (url) => fetch(url)
-    .then(response => {
+      ...params,
+    }),
+  );
+};
+export const get = (url) =>
+  fetch(url)
+    .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
       return response.json();
     })
-    .catch(error => {
-      console.error('Fetch error:', error);
+    .catch((error) => {
+      console.error("Fetch error:", error);
       throw error;
     });
 
 export const assertEqual = (a, b) => {
   if (development) {
     if (JSON.stringify(a) !== JSON.stringify(b)) {
-      console.error('values are not equal', a, b)
+      console.error("values are not equal", a, b);
     }
   }
-}
+};
--- script.js
+++ script.js
@@ -1,67 +1,68 @@
-import {get, div, button, input, renderComponent, a} from './helpers.js'
-import {ServerList} from './serverlist.js'
+import { get, div, button, input, renderComponent, a } from "./helpers.js";
+import { ServerList } from "./serverlist.js";
 
-const App = ({state, setState}) => {
-  const userName = state.userName || {error: "Please enter a username"}
+const App = ({ state, setState }) => {
+  const userName = state.userName || { error: "Please enter a username" };
   if (userName.error === undefined) {
-    return Servers({userName, state, setState})
+    return Servers({ userName, state, setState });
   } else {
     return LoginForm({
       name: userName,
-      setName:(name) => {
-        setState({userName: name, favourites: state.favourites || [name.server]})
-      }
-    })
+      setName: (name) => {
+        setState({
+          userName: name,
+        });
+      },
+    });
   }
-}
+};
 
-const Servers = ({userName, state, setState}) => {
-  return div({className: 'container'}, [
-    div({className:'header'}, [
-    div({text: `Hello ${userName.handle}`}),
-    button({
-      text: "Quit", 
-      onClick: () => 
-        setState({userName: localStorage.removeItem('userName')})
-    }),
-
+const Servers = ({ userName, state, setState }) => {
+  console.log("Rendering server list", state);
+  return div({ className: "container" }, [
+    div({ className: "header" }, [
+      div({ text: `Hello ${userName.handle}` }),
+      button({
+        text: "Quit",
+        onClick: () =>
+          setState({ userName: localStorage.removeItem("userName") }),
+      }),
     ]),
 
     ServerList({
-      page: state.page || 0, 
-      pageSize: state.pageSize || 3, 
-      list: state.favourites || [], 
-      addServer: (name) => 
-        setState({
-          favourites: (state.favourites || []).concat([name])
-        }), 
+      state: state.favourites,
+      setState: (favourites) => setState({ favourites }),
       userName,
     }),
-    div({className:'footer'}, [])
-  ])
-}
+    div({ className: "footer" }, []),
+  ]);
+};
 
-const LoginForm = ({name, setName}) => {
+const LoginForm = ({ name, setName }) => {
   const parseName = (name) => {
-    const [prefix, handle, server] = name.split('@')
-    if (prefix !== '') {
-      return {error: `Your handle is invalid, handles should begin with "@", yours begins with "${prefix}`}
+    const [prefix, handle, server] = name.split("@");
+    if (prefix !== "") {
+      return {
+        error: `Your handle is invalid, handles should begin with "@", yours begins with "${prefix}`,
+      };
     } else if (/\./.test(handle)) {
-      return {error: `Your handle is invalid, "${handle} contains special characters, it should be alphanumeric`}
+      return {
+        error: `Your handle is invalid, "${handle} contains special characters, it should be alphanumeric`,
+      };
     } else if (server === undefined) {
-      return {error: `Your handle is invalid, it does not contain the server URL`}
+      return {
+        error: `Your handle is invalid, it does not contain the server URL`,
+      };
     } else {
-      return {handle, server}
+      return { handle, server };
     }
-  }
-  const field = input({placeholder: 'Write your server'})
+  };
+  const field = input({ placeholder: "Write your server" });
   return div({}, [
-    div({text: name.error}),
+    div({ text: name.error }),
     field,
-    button({text: "Go", onClick: () => setName(parseName(field.value))})
-  ])
-}
+    button({ text: "Go", onClick: () => setName(parseName(field.value)) }),
+  ]);
+};
 
-
-
-window.onload = renderComponent(App)
+window.onload = renderComponent(App);
